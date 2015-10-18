9679066d5db20d5afa1050627b3a70434f92b3fe
Drew Christensen <drew@giantrobotpilot.com>
2015-10-18 14:51:29 -0600

    Editing view controllers

--- Clock Clone/AlarmViewController.swift
+++ Clock Clone/AlarmViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AlarmViewController: UIViewController {
+class AlarmViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Clock Clone/WorldClockViewController.swift
+++ Clock Clone/WorldClockViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WorldClockViewController: UIViewController {
+class WorldClockViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
